2d514f0bd3523dee2f86c520d37b46ba7579e705
Junior Barros <juniorbarros@sysfactech.com.br>
2024-07-25 16:15:24 -0300

    Atualizar dependências e configuração do Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -4,17 +4,20 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-# Configuration for Sphinx
+# Project information
 project = 'My Software'
 author = 'Your Name'
 release = '0.1'
 
+# Extensions
 extensions = [
     'sphinx_rtd_theme',
 ]
 
+# Paths
 templates_path = ['_templates']
 exclude_patterns = []
 
+# HTML output
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
